4a2b0a2dd8d32fcd380f2592fc8c10f76cf23134
Jacek Iwański <jiwanski@users.noreply.github.com>
2020-04-02 14:50:41 +0200

    no headless mode for now

--- reverso_conjugation/fixtures/driver.py
+++ reverso_conjugation/fixtures/driver.py
@@ -5,7 +5,7 @@ from selenium import webdriver
 @pytest.fixture(scope="class")
 def chrome_headless(request):
     options = webdriver.ChromeOptions()
-    options.add_argument('--headless')
+    # options.add_argument('--headless')
     options.add_argument("--proxy-server='direct://'")
     options.add_argument('--proxy-bypass-list=*')
     driver = webdriver.Chrome(options=options, service_log_path='/dev/null')
